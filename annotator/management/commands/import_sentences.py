8a0b15a609afa2c24d02bf0de0f44b2b6be3e928
Shaform <shaform@gmail.com>
2015-01-06 16:23:12 +0800

    update tools

--- annotator/management/commands/import_sentences.py
+++ annotator/management/commands/import_sentences.py
@@ -2,7 +2,8 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db.transaction import get_autocommit, set_autocommit, commit
 from django.db import IntegrityError
 
-from annotator.models import Sentence
+from annotator.models import Sentence, PairConnective, SingleConnective
+from annotator.views import analyse_tokens
 
 class Command(BaseCommand):
     args = '<input_path ..>'
@@ -17,8 +18,13 @@ class Command(BaseCommand):
                     for l in f:
                         try:
                             source_id, text = l.rstrip().split('\t')
-                            sent = Sentence.objects.create(source_id=source_id, text=text)
-                            sent.save()
+                            tokens = text.strip().split(' ')
+                            targets, _, _ = analyse_tokens(tokens)
+                            if len(targets) > 0:
+                                sent = Sentence.objects.create(source_id=source_id, text=text)
+                                sent.save()
+                            else:
+                                print(source_id, 'does not have connectives')
                         except IntegrityError:
                             print(source_id, 'cannot be inserted')
             commit()
